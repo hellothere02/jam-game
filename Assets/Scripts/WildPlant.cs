149eaa33115cedb368651a268de8398e8b7a57d8
Pashkov Mark <mdpashkov@mail.ru>
2022-11-11 00:37:32 +0300

    upgrade platforms and other stuff

--- Assets/Scripts/WildPlant.cs
+++ Assets/Scripts/WildPlant.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class WildPlant : MonoBehaviour
 {
     [SerializeField] private Animator anim;
+    private BoxCollider2D boxCollider2;
     private bool isdeathWild = false;
     public static event OnDeathWild DeathWild;
     public delegate void OnDeathWild(bool death);
 
+    private void Start()
+    {
+        boxCollider2 = GetComponent<BoxCollider2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        boxCollider2.enabled = false;
         anim.SetBool("Clap", true);
         isdeathWild = true;
         DeathWild(isdeathWild);
